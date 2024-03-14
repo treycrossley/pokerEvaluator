55b05b744676c9ad105d37c17d505536a4906bc1
Trey Crossley <treycrossley@gmail.com>
2024-03-14 08:54:26 -0700

    minor changes

--- poker.py
+++ poker.py
@@ -1,24 +1,5 @@
 from models.deck import Deck
-from models.card import Card
 import services.hand_evaluator_service as hand_evaluator_service
-
-test_hands = [
-    ["Ac", "5c", "10c", "7c", "3s"],
-    ["2c", "3d", "4s", "5h", "2d"],
-    ["2c", "3d", "4s", "3h", "2d"],
-    ["5s", "4c", "Ad", "4s", "4h"],
-    ["3h", "7h", "6s", "4d", "5s"],
-    ["Ac", "5c", "10c", "7c", "3c"],
-    ["5c", "8d", "5h", "8s", "8h"],
-    ["3d", "7h", "7s", "7c", "7d"],
-    ["5c", "4c", "8c", "7c", "6c"],
-    ["As", "10s", "Qs", "Ks", "Js"],
-    ["10c", "Jc", "Qc", "Ac", "Kc"],
-    ["3h", "5h", "Qs", "9h", "Ac"],
-    ["10s", "10c", "Ah", "10d", "10h"]
-]
-
-test_players = ["Westley", "Buttercup", "Inigo", "Fezzik"]
 
 # Part 1
 def hand_ranking(hand): 
@@ -66,7 +47,7 @@ def main():
     """Root of program
     Returns: Ideally nothing if execution wasn't halted by an error in the program
     """
-    round = deal_cards(test_players)
+    round = deal_cards(["Johnny", "Timmy", "Jerry", "BARTHOLOMEW"])
     print(winner_is(round))
 
 
